349aef13c4d2b647e61ef616ed2bc56e62f7a60e
Vladimir-Aires <vlad.aires05@gmail.com>
2025-11-21 10:26:00 -0300

    Validador de Filial Pronto

---
+++ src/validators/FilialValidator.js
@@ -0,0 +1,24 @@
+const yup = require("yup");
+const mongoose = require("mongoose");
+
+const schema = yup.object().shape({
+    nome: yup.string().required(),
+    cnpj: yuyp.number().required(),
+    telefone: yup.number().required(),
+    email: yup.string().email().required(),
+    gerente: yup.string().required().test("id-validator", "id do gerente é inválido", (value) => mongoose.Types.ObjectId.isValid(value))
+    ,
+    status: yup.string().required()
+    
+});
+
+async function validarFilial(req, res, next) {
+    try {
+        await schema.validate(req.body, { abortEarly: false });
+        next();
+    } catch (error) {
+        return res.status(400).json({ erros: error.errors });
+    }
+}
+
+module.exports = { validarFilial };
